1a31717135f2a445ad70205afec4395098333166
Charlotte Chang <yellowspiderlove@gmail.com>
2014-02-13 18:14:35 -0500

    some minor changes to flow

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Shorty::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'urls#index'
+  root 'urls#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
